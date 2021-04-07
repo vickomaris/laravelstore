5f620b2f4649ee8d12137f0446144de11b7eed70
vickomaris <vicko.maris.s@gmail.com>
2021-04-08 01:17:54 +0700

    firstcommit

---
+++ resources/views/pages/admin/product/index.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.admin')
+
+@section('title')
+    Product
+@endsection
+
+@section('content')
+ <!-- section Content -->
+<div
+class="section-content section-dashboard-home"
+data-aos="fade-up"
+>
+    <div class="container-fluid">
+        <div class="dashboard-heading">
+        <h2 class="dashboard-title">Product</h2>
+        <p class="dashboard-subtitle">List of Products</p>
+        </div>
+        <div class="dashboard-content">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <a href="{{ route('product.create') }}" class="btn btn-primary mb-3">
+                                + Tambah Product Baru
+                            </a>
+                            <div class="table-responsive">
+                                <table class="table table-hover scroll-horizontal-vertical w-100" id="crudTable">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Nama</th>
+                                            <th>Pemilik</th>
+                                            <th>Kategori</th>
+                                            <th>Harga</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody></tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('addon-script')
+    <script>
+        var datatable = $('#crudTable').DataTable({
+            processing: true,
+            serverSide: true,
+            ordering: true,
+            ajax: {
+                url: '{!! url()->current() !!}',
+            },
+            columns:[
+                {data: 'id', nama: 'id'},
+                {data: 'name', nama: 'name'},
+                {data: 'user.name', nama: 'user.name'},
+                {data: 'category.name', nama: 'category.name'},
+                {data: 'price', nama: 'price'},
+                {
+                    data: 'action',
+                    nama: 'action',
+                    orderable: false,
+                    searcable: false,
+                    width:'15%'
+                },
+            ]
+        })
+    </script>
+@endpush
+
---
+++ routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/categories', 'CategoryController@index')->name('categories');
+Route::get('/categories/{id}', 'CategoryController@detail')->name('categories-detail');
+Route::get('/details/{id}', 'DetailController@index')->name('detail');
+Route::post('/details/{id}', 'DetailController@add')->name('detail-add');
+Route::get('/cart', 'CartController@index')->name('cart');
+Route::delete('/cart/{id}', 'CartController@delete')->name('cart-delete');
+Route::get('/success', 'CartController@success')->name('success');
+
+Route::get('/register/success', 'Auth\RegisterController@success')->name('register-success');
+
+Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+
+Route::get('/dashboard/products', 'DashboardProductController@index')
+    ->name('dashboard-product');
+Route::get('/dashboard/products/create', 'DashboardProductController@create')
+    ->name('dashboard-product-create');
+Route::get('/dashboard/products/{id}', 'DashboardProductController@details')
+    ->name('dashboard-product-details');
+
+Route::get('/dashboard/transactions', 'DashboardTransactionController@index')
+    ->name('dashboard-transaction');
+Route::get('/dashboard/transactions/{id}', 'DashboardTransactionController@details')
+    ->name('dashboard-transaction-details');
+
+Route::get('/dashboard/settings', 'DashboardSettingController@store')
+    ->name('dashboard-settings-store');
+Route::get('/dashboard/account', 'DashboardSettingController@account')
+    ->name('dashboard-settings-account');
+
+//->middleware(['auth','admin'])
+
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->group(function(){
+        Route::get('/', 'DashboardController@index')->name('admin-dashboard');
+        Route::resource('category', 'CategoryController');
+        Route::resource('user', 'UserController');
+        Route::resource('product', 'ProductController');
+        Route::resource('product-gallery', 'ProductGalleryController');
+    });
+
+
+Auth::routes();
+
+
